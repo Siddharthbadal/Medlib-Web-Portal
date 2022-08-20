ceeb130469ed7cdf4b91e164c140dbc3e5599428
Siddharth <siddharthabadal.2015@gmail.com>
2022-08-20 15:08:22 +0530

    deploy

--- app/models.py
+++ app/models.py
@@ -6,14 +6,14 @@ class Patients(models.Model):
         ('M', 'M'),
         ('F', 'F'),
     )
-    id = models.IntegerField(primary_key=True, null=True)
-    name = models.CharField(max_length=60)
-    phone = models.CharField(max_length=20)
-    email = models.CharField(max_length=60)
-    age = models.CharField(max_length=60)
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=60, null=True, blank=True,)
+    phone = models.CharField(max_length=20, null=True, blank=True,)
+    email = models.CharField(max_length=60, null=True, blank=True,)
+    age = models.CharField(max_length=60, null=True, blank=True,)
     gender = models.CharField(max_length=1, null=True, blank=True, choices=GENDER)
     note = models.TextField()
-    created_at=models.DateTimeField(auto_now_add=True)
+    created_at=models.DateTimeField(auto_now_add=True, null=True, blank=True,)
 
     def __str__(self) -> str:
         return self.name
