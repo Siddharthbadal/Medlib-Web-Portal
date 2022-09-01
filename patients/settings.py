22aba8c72687f9883fb9ad9223efb009dd8a2fd8
Siddharth badal <siddharthabadal.2015@gmail.com>
2022-09-01 14:16:23 +0530

    Update settings.py

--- patients/settings.py
+++ patients/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%cje8g+@lmfbp^py^#mj5l@9-_9wo+!kd-perohxv3&!6isp8k'
+SECRET_KEY = 'django-insecure-**************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
